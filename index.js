098345c39428e1e05cea9cb939e8e362fa5e6158
Poonam229 <poonam.chauhan229@gmail.com>
2023-10-10 18:05:33 +0530

    cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const _ = require('lodash');
-const port = 3000;
+const port = process.env.PORT ||3000 ;
 const cors=require('cors')
 const blogMiddleware = require('./middleware/auth')
 
